910600fc72f6b8d4f31f5c1d2c6fc95c019c720d
Henrique Oliveira <henriqueoliveira101@hotmail.com>
2019-02-15 13:55:05 -0500

    Added config.js
    Added code to get config to run

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,8 +2,8 @@
 
 let apiUrl
 const apiUrls = {
-  production: '<replace-with-heroku-url>',
-  development: 'http://localhost:4741'
+  production: 'https://tic-tac-toe-wdi.herokuapp.com/',
+  development: 'http://localhost:7165'
 }
 
 if (window.location.hostname === 'localhost') {
