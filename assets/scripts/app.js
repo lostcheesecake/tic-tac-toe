913eb1fca027c48320c45a05e2beb37c6d59bd6c
Henrique Oliveira <henriqueoliveira101@hotmail.com>
2019-02-15 13:52:54 -0500

    Added code to app.js
    Added function for x and o

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,4 +1,6 @@
 'use strict'
+const store = require('./store.js')
+const events = require('./books/events.js')
 
 // use require with a reference to bundle the file and use it in this file
 // const example = require('./example')
@@ -7,5 +9,20 @@
 // require('./example')
 
 $(() => {
-  // your JS code goes here
+  $('.cell').on('click', function () {
+    if (store.user === 'X') {
+      store.user = 'O'
+    } else {
+      store.user = 'X'
+    }
+    $(this).text(store.user)
+    console.log(store.user)
+  })
+})
+
+$(() => {
+  $('#sign-up-form').on('submit', events.onSignUp)
+  $('#sign-in-form').on('submit', events.onSignIn)
+  $('#change-password-form').on('submit', events.onChangePassword)
+  $('#sign-out-form').on('submit', events.onSignOut)
 })
