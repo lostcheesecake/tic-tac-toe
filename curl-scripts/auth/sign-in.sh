9c958eed10f98ace3752de73a1764bb633afc2c6
Henrique Oliveira <henriqueoliveira101@hotmail.com>
2019-02-20 09:11:14 -0500

    Added code to curl-scripts, sign-in, added code to index.html

--- curl-scripts/auth/sign-in.sh
+++ curl-scripts/auth/sign-in.sh
@@ -2,6 +2,7 @@ curl "https://wdi-library-api.herokuapp.com/sign-in" \
   --include \
   --request POST \
   --header "Content-Type: application/json" \
+  --header "Authorization: Token token=${TOKEN}" \
   --data '{
     "credentials": {
       "email": "'"${EMAIL}"'",
